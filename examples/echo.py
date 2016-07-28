9468c1e046745c7fc6e8f71ea8db36a8477f95c3
mehdi gmira <mgmira@wiremind.fr>
2016-07-28 19:06:42 +0200

    not working

--- examples/echo.py
+++ examples/echo.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, division
 
+from time import sleep
+
 
 def run():
     while True:
-        x = (yield)
-        if x is None:
+        x = (yield)  # get data from storage
+        if x is None:  # first run, initialize
             x = 0
         if x > 10:
             break
-        yield x + 1
+        yield x + 1  # save data
+        print x + 1
+        sleep(2)
+        print 'yo'
--- run.py
+++ run.py
@@ -3,28 +3,39 @@ from __future__ import unicode_literals, division
 
 import argparse
 import imp
+from time import sleep
 
 
-def import_function(path):
-    contained_module = imp.load_source('contained', path)
-    contained_module.run()
-
+def run_remote(data=None):
+    try:
+        func = imp.load_source('contained', args.file_path).run()
+        func.next()
+        while True:
+            data = func.send(data)
+            func.next()
+    except StopIteration:
+        return StopIteration
+    except KeyboardInterrupt:
+        return data
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='run cooperative code')
     parser.add_argument('file_path', metavar='file_path', type=str, help='file to run')
     args = parser.parse_args()
-    contained_module = imp.load_source('contained', args.file_path)
-    func = contained_module.run()
-    func.next()
-    x = None
+    data = None
     while True:
-        # saved_stuff = func.next()
-        # func.next()
-        x = func.send(x)
-        func.next()
-        print x
-        # func.next()
-        # print saved_stuff
+        data = run_remote(data)
+        if data is StopIteration:
+            print 'byebye'
+            break
+        else:
+            print 'You have 2s to Keyboard interrupt the program for real. Otherwise, it will be reloaded and resumed'
+            try:
+                sleep(2)
+            except KeyboardInterrupt:
+                print 'byebye'
+                break
+
+
 
 
