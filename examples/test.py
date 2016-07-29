cae06a65c9430480c5bd6c02812d2311093fd0db
mehdi gmira <mgmira@wiremind.fr>
2016-07-29 10:04:11 +0200

    refactor

--- examples/test.py
+++ examples/test.py
@@ -3,21 +3,24 @@ from __future__ import unicode_literals, division
 
 from time import sleep
 
+data_init = range(20)
+
 
 def run():
     while True:
         data = (yield)  # get data from storage
 
         if data is None:  # first run, initialize
-            data = 0
+            data = data_init
 
-        if data > 10:  # break condition
+        if not data:  # break condition
             break
 
         # Logic goes here
-        data += 1
-        print 'that is ', data
-        sleep(2)
+        time_sleep = 2
+        print data.pop()
+        print 'sleeping %ss' % time_sleep
+        sleep(time_sleep)
         ###
 
         yield data  # save data
